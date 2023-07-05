1b98f539a9813d308920ac737bbf96b6a6ffce90
DESKTOP-0RI489M\PC <khjmdhk@gmail.com>
2023-07-05 23:49:15 +0900

    Callby

---
+++ CallbyTest.java
@@ -0,0 +1,18 @@
+class CallbyTest
+{
+	public static void main(String[] args){
+		int a = 10;
+		Car mycar = new Car("Father", 500);
+
+		System.out.println("Call-by-value");
+		System.out.println("befor calling: a = "+a);
+		Callby.value(a);
+		System.out.println("after calling: a = "+a);
+
+		System.out.println();
+		System.out.println("call-by-reference");
+		System.out.println("befor calling: mycar.price = "+mycar.price+"\t mycar.owner = "+mycar.owner);
+		Callby.ref(mycar);
+		System.out.println("after calling: mycar.price = " + mycar.price+"\t mycar.owner = "+mycar.owner);
+	}
+}
